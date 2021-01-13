0224500e73a0e4d48a194ab02fa95605a0c51e40
����ɽ <544610616@qq.com>
2021-01-13 12:58:26 +0800

    1

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="cantainer">
-  </div>
+  <el-collapse v-model="activeNames" @change="handleChange">
+    <el-collapse-item title="tianzhushan" @click.native="clickItem()">
+      <div>邮箱：tian.zhu.shan@qq.com</div>
+      <div>微信号：wxid_rsm5kkpepdw821</div>
+    </el-collapse-item>
+  </el-collapse>
 </template>
-
 <script>/* eslint-disable */
   export default {
-    name: 'Card',
     data () {
       return {
+        activeNames: 'tianzhushan'
       }
     },
-    created() {
-    },
     methods: {
+      handleChange:function () {
+        console.log("1")
+      },
+      clickItem:function () {
+        console.log("2")
+      }
     }
   }
 </script>
